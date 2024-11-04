37d7c56d3713764f2625f4be46f5b59b6e4bb451
Webosmotic <webosmotic@Webosmotics-Mac-mini.local>
2024-11-04 12:25:01 +0530

    Updates

--- src/components/sample/lib/config.js
+++ src/components/sample/lib/config.js
@@ -1,6 +1,6 @@
 const config = {
   initiateLoginEndpoint:
-    "https://daa5-2405-201-2009-d150-f41a-aa21-abd6-635a.ngrok-free.app",
+    "https://msteams-poc-react.onrender.com/auth-start.html",
   clientId: "9334b7c5-1fe7-43ce-832b-d8b90af6d1ed",
   apiName: process.env.REACT_APP_FUNC_NAME,
   // initiateLoginEndpoint: process.env.REACT_APP_START_LOGIN_PAGE_URL,
