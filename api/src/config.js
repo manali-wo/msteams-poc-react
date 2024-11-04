ec3ef115a649b08ac99baa1f006d75a7c3b67952
Webosmotic <webosmotic@Webosmotics-Mac-mini.local>
2024-11-04 15:40:36 +0530

    Resolved: moved env variables to code

--- api/src/config.js
+++ api/src/config.js
@@ -1,6 +1,6 @@
 const config = {
   authorityHost: "https://login.microsoftonline.com",
-  tenantId: "c6cb36b7-8111-4950-8003-ca43369dc469",
+  tenantId: "9188040d-6c67-4c5b-b112-36a304b66dad",
   clientId: "9334b7c5-1fe7-43ce-832b-d8b90af6d1ed",
   clientSecret: "4f7a0388-dba4-46d7-8453-c2828dd0060a",
 };
