5810c7e4625f14c78054a0824420788aae173faa
Webosmotic <webosmotic@Webosmotics-Mac-mini.local>
2024-11-04 10:20:30 +0530

    Resolved: moved env variables to code

--- src/components/sample/lib/config.js
+++ src/components/sample/lib/config.js
@@ -1,8 +1,13 @@
 const config = {
-  initiateLoginEndpoint: process.env.REACT_APP_START_LOGIN_PAGE_URL,
-  clientId: process.env.REACT_APP_CLIENT_ID,
+  initiateLoginEndpoint:
+    "https://msteams-poc-react.onrender.com/auth-start.html",
+  clientId: "9334b7c5-1fe7-43ce-832b-d8b90af6d1ed",
   apiEndpoint: process.env.REACT_APP_FUNC_ENDPOINT,
   apiName: process.env.REACT_APP_FUNC_NAME,
+  // initiateLoginEndpoint: process.env.REACT_APP_START_LOGIN_PAGE_URL,
+  // clientId: process.env.REACT_APP_CLIENT_ID,
+  // apiEndpoint: process.env.REACT_APP_FUNC_ENDPOINT,
+  // apiName: process.env.REACT_APP_FUNC_NAME,
 };
 
 export default config;
